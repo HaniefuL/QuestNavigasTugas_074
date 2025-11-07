f186dcf62cdf5c94889453a6aef898f060538df5
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 19:33:06 +0700

    menambahkan composable FormulirPendaftaran dengan parameter onSubmitClicked dan onBackPressed

--- app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirPendaftaran(
+    onSubmitClicked: () -> Unit,
+    onBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+)
