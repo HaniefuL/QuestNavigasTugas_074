79c84e95f3ffbdc6f41843efa5bb8c0f1af9bbe2
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 19:35:45 +0700

    menambahkan state input (nama, alamat, jenisKelamin, statusPerkawinan) dan resource dimensi pada FormulirPendaftaran

--- app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
@@ -1,11 +1,27 @@
 package com.example.questnavigastugas_074.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun FormulirPendaftaran(
     onSubmitClicked: () -> Unit,
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier
-)
+){
+    var nama by remember { mutableStateOf(TextFieldValue("")) }
+    var alamat by remember { mutableStateOf(TextFieldValue("")) }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
+
+    val paddingSmall = dimensionResource(id = R.dimen.padding_small)
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+    val paddingLarge = dimensionResource(id = R.dimen.padding_large)
+    val buttonHeight = dimensionResource(id = R.dimen.button_height)
+}
