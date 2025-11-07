f69b089ac47b063fa5908f48297c57bb0d1d7d60
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 19:50:42 +0700

    menambahkan verticalScroll pada Column

--- app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
@@ -1,12 +1,16 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
@@ -31,5 +35,14 @@ fun FormulirPendaftaran(
     Surface(
         color = Color(0xFFF6ECFF),
         modifier = modifier.fillMaxSize() // Gunakan modifier dari parameter
-    )
+    ){
+        {
+            Column(
+                // PERBAIKAN 2: Tambahkan verticalScroll agar konten tidak terpotong
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+    }
 }
