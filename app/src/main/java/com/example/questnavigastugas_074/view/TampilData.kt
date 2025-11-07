8fd8fc97d9313a1d069a7aa76a08d294bb9efd25
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:03:40 +0700

    enambahkan teks judul "List Daftar Peserta" pada composable TampilData

--- app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TampilData(navController: NavHostController){
@@ -27,5 +30,12 @@ fun TampilData(navController: NavHostController){
                 .fillMaxSize()
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){}
+        ){
+            Text(
+                text = "List Daftar Peserta",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
 }
