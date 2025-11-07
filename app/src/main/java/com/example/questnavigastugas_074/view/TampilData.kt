40fe6c8915ac22d69a3a174ea7dfb0cc2be9efd6
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:02:13 +0700

    menambahkan komponen Surface dan Column dengan latar gradient serta padding pada TampilData

--- app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
@@ -1,9 +1,17 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TampilData(navController: NavHostController){
@@ -11,4 +19,13 @@ fun TampilData(navController: NavHostController){
     val gradient = Brush.verticalGradient(
         colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
     )
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){}
 }
