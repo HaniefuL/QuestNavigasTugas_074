d6fc95f55e1b2163565ce4e7293ae8b5d647c33e
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:13:17 +0700

    menambahkan konten WelcomeScreen berisi teks sambutan, gambar, serta tombol navigasi menuju Formulir dan TampilData

--- app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
@@ -1,14 +1,25 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(
@@ -22,5 +33,46 @@ fun WelcomeScreen(
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
+        ) {
+            Text("Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = "image",
+                modifier = Modifier.size(200.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Hanieful Geofarry Juneo Syahyana", fontSize = 25.sp)
+            Text(
+                "20230140074", fontSize = 23.sp,
+                color = MaterialTheme.colorScheme.secondary
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = onFormulirPendaftaranClicked,
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp)
+            ) {
+                Text("Buka Formulir Pendaftaran")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = onTampilDataClicked,
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(50.dp)
+            ) {
+                Text("Lihat Daftar Peserta")
+            }
+        }
+    }
 }
