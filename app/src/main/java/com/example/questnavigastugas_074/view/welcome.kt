c183adc860de65cc3788567f45d0838d25563289
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:11:15 +0700

    menambahkan komponen Surface dan Column dengan tata letak terpusat pada composable WelcomeScreen

--- app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
@@ -1,9 +1,26 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeScreen(
     onTampilDataClicked: () -> Unit,
     onFormulirPendaftaranClicked: () -> Unit
-) {}
+) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+}
