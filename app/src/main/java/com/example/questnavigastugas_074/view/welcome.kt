f8e86af91bb19bde04e18b77a55ed7e22d3ce7dc
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:09:50 +0700

    menambahkan composable WelcomeScreen dengan parameter navigasi ke TampilData dan FormulirPendaftaran

--- app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/welcome.kt
@@ -1,2 +1,9 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WelcomeScreen(
+    onTampilDataClicked: () -> Unit,
+    onFormulirPendaftaranClicked: () -> Unit
+) {}
