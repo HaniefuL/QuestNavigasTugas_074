6a185448d165ed6c1b5e39c9aba946a5b6a79421
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 19:48:33 +0700

    menambahkan komponen Surface dengan warna latar #F6ECFF dan modifier fillMaxSize pada FormulirPendaftaran

--- app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/Formulir.kt
@@ -1,11 +1,14 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.input.TextFieldValue
 
@@ -24,4 +27,9 @@ fun FormulirPendaftaran(
     val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
     val paddingLarge = dimensionResource(id = R.dimen.padding_large)
     val buttonHeight = dimensionResource(id = R.dimen.button_height)
+
+    Surface(
+        color = Color(0xFFF6ECFF),
+        modifier = modifier.fillMaxSize() // Gunakan modifier dari parameter
+    )
 }
