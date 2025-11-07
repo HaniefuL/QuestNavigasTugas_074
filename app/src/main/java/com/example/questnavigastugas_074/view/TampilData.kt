c758b0a92c637b061f40c3a09368f9cfc0666731
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:01:19 +0700

    menambahkan composable TampilData dengan stringArrayResource data peserta dan background gradient

--- app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
@@ -1,2 +1,14 @@
 package com.example.questnavigastugas_074.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringArrayResource
+
+@Composable
+fun TampilData(navController: NavHostController){
+    val pesertaList = stringArrayResource(id = R.array.data_peserta)
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
+    )
+}
