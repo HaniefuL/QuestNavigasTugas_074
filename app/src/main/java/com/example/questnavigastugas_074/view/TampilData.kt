f5c68bdd1beddc487c30e7bf4104a7bf63ce7c12
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-07 20:06:34 +0700

    menampilkan data peserta dalam bentuk Card di dalam LazyColumn pada composable TampilData

--- app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_074/view/TampilData.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,7 +54,18 @@ fun TampilData(navController: NavHostController){
                     .weight(1f), // Agar LazyColumn mengisi ruang yang tersedia
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ){
-
+                items(pesertaList) { peserta ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(16.dp),
+                        elevation = CardDefaults.cardElevation(8.dp)
+                    ) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(text = peserta, fontSize = 16.sp)
+                        }
+                    }
+                }
             }
         }
 }
